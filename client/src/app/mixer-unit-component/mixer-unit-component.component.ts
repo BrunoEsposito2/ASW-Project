aa1f203026742302561e86d7c0d815df88dc8792
Agnoletti <agnohendrix@gmail.com>
2022-09-26 16:30:24 +0200

    changed layout | added basic canvas drawing

--- client/src/app/mixer-unit-component/mixer-unit-component.component.ts
+++ client/src/app/mixer-unit-component/mixer-unit-component.component.ts
@@ -1,20 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-mixer-unit-component',
   template: `
-    <p>
-      mixer-unit-component works!
-    </p>
+    <canvas class="" #canvas ></canvas>
+    <button (click)="animate()">Play</button>
   `,
   styles: [
+    `p {
+      color:blue;
+    }
+    canvas {
+      border-style: solid;
+      width: 400px;
+    }
+    `
   ]
 })
 export class MixerUnitComponentComponent implements OnInit {
 
+  @ViewChild('canvas', { static: true })
+  canvas!: ElementRef<HTMLCanvasElement>;
+  private ctx!: CanvasRenderingContext2D | null;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+  }
+
+  animate(): void {
+    this.ctx!.fillStyle = 'red';
+  //const square = new Square(this.ctx);
+  this.ctx!.fillRect(5, 1, 20, 20);
   }
 
 }
