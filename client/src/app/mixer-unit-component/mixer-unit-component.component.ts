5099c0df19e0de8d76949e486ae6c615ea1d4aeb
Agnoletti <agnohendrix@gmail.com>
2022-10-03 16:53:54 +0200

    Added basic industry map | movement for employees

--- client/src/app/mixer-unit-component/mixer-unit-component.component.ts
+++ client/src/app/mixer-unit-component/mixer-unit-component.component.ts
@@ -5,7 +5,7 @@ import { EmployeeRenderedComponent } from '../render/employee-rendered.component
 @Component({
   selector: 'app-mixer-unit-component',
   template: `
-    <canvas class="" #canvas ></canvas>
+    <canvas class="" #canvas width=300 height=150></canvas>
     <button (click)="animate()">Play</button>
   `,
   styles: [
@@ -14,13 +14,15 @@ import { EmployeeRenderedComponent } from '../render/employee-rendered.component
     }
     canvas {
       border-style: solid;
-      width: 400px;
+      width: 1000px;
+      overflow: scroll;
     }
     `
   ]
 })
 export class MixerUnitComponentComponent implements OnInit {
 
+  //Canvas is 300*150 (as coordinates)
   @ViewChild('canvas', { static: true })
   canvas!: ElementRef<HTMLCanvasElement>;
   private ctx!: CanvasRenderingContext2D | null;
@@ -29,14 +31,27 @@ export class MixerUnitComponentComponent implements OnInit {
 
   ngOnInit(): void {
     this.ctx = this.canvas.nativeElement.getContext('2d');
+
+    //Draw industry map
+    this.ctx!.fillStyle = 'black';
+
+    this.ctx!.strokeRect(20, 10, 60, 60);
+    this.ctx!.fillText("Macchina 1", 24, 40);
+    this.ctx!.strokeRect(120, 10, 60, 60);
+    this.ctx!.fillText("Macchina 2", 124, 40);
+    this.ctx!.strokeRect(220, 10, 60, 60);
+    this.ctx!.fillText("Macchina 3", 224, 40);
+
+    //TO-DO add listeners to machines
   }
 
   animate(): void {
+
     const employee = new EmployeeRenderedComponent(this.ctx!);
     this.ctx!.fillStyle = 'red';
-  //const square = new Square(this.ctx);
-  //this.ctx!.fillRect(5, 1, 20, 20);
-  employee.draw(1, 1, 1);
+
+    employee.draw(290, 140, 10);
+    employee.move(10, 10);
   }
 
 }
--- client/src/app/render/employee-rendered.component.ts
+++ client/src/app/render/employee-rendered.component.ts
@@ -14,8 +14,22 @@ export class EmployeeRenderedComponent implements OnInit {
   constructor(private ctx: CanvasRenderingContext2D) {}
 
    draw(x: number, y: number, z: number) {
-     this.ctx.fillRect(z * x, z * y, z, z);
+     this.ctx.fillRect(x, y, z, z);
    }
+
+   move(y: number, z: number) {
+    const max = this.ctx.canvas.width / z;
+    const canvas = this.ctx.canvas;
+    let x = 0;
+    const i = setInterval(() => {
+      this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+      this.draw(x, y, z);
+      x++;
+      if (x >= max) {
+        clearInterval(i);
+      }
+    }, 200);
+  }
 
   ngOnInit(): void {
   }
