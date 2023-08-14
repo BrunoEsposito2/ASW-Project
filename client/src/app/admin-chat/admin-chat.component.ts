1cc747f9bfa13d8e7d5c3604a67cc88b283c7277
BrunoEsposito2 <brunoesposito96@gmail.com>
2023-08-14 13:06:36 +0200

    add chat and navbar components for admins and employees

---
+++ client/src/app/admin-chat/admin-chat.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-chat',
+  template: `
+    <app-admin-navbar></app-admin-navbar>
+    <app-chat></app-chat>
+  `,
+  styles: [
+  ]
+})
+export class AdminChatComponent {
+
+}
---
+++ client/src/app/employee-chat/employee-chat.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-chat',
+  template: `
+    <app-employee-navbar></app-employee-navbar>
+    <app-chat></app-chat>
+  `,
+  styles: [
+  ]
+})
+export class EmployeeChatComponent {
+
+}
