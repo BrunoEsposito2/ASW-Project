ef4890f36e6533d49b0938a716ff6df60b50aa41
Agnohendrix <agnohendrix@gmail.com>
2023-12-05 17:18:09 +0100

    Added Employee shape

--- client/src/app/render/EmployeeShape.ts
+++ client/src/app/render/EmployeeShape.ts
@@ -12,17 +12,59 @@ export class EmployeeShape extends Shape {
         super();
         this.stage = stage;
         this.layer = layer;
-        var rect = new Konva.Rect({
-            fill: 'blue',
-            x: this.stage.width() - 100,
-            y: this.stage.height() - 250,
-            width: 100,
-            height: 100,
+
+        var circle = new Konva.Circle({
+            stroke: 'black',
+            x: this.stage.width() - 50,
+            y: this.stage.height() - 200,
+            radius: 50
         });
-        rect.on('mouseover', function(){
+        circle.on('click', function(){
             console.log("over emplo");
         });
-        this.layer.add(rect);
+        
+        var righteye = new Konva.Circle({
+            stroke: 'black',
+            x: this.stage.width() - 30,
+            y: this.stage.height() - 220,
+            radius: 10
+        });
+        var lefteye = new Konva.Circle({
+            stroke: 'black',
+            x: this.stage.width() - 70,
+            y: this.stage.height() - 220,
+            radius: 10
+        });
+
+        //Happy
+        var arc = new Konva.Arc({
+            x: this.stage.width() - 50,
+            y: this.stage.height() - 210,
+            innerRadius: 30,
+            outerRadius: 40,
+            angle: 90,
+            fill: 'green',
+            stroke: 'black',
+            strokeWidth: 4,
+            rotation: 45
+        });
+
+        //Sad
+        var arc = new Konva.Arc({
+            x: this.stage.width() - 50,
+            y: this.stage.height() - 150,
+            innerRadius: 30,
+            outerRadius: 40,
+            angle: 90,
+            fill: 'red',
+            stroke: 'black',
+            strokeWidth: 4,
+            rotation: 225
+        });
+        this.layer.add(arc);
+        this.layer.add(lefteye);
+        this.layer.add(righteye);
+        this.layer.add(circle);
     }
     
 }
