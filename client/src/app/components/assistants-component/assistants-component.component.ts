a3770a8f7ee97c75b850c10073a562b3b584587d
BrunoEsposito2 <brunoesposito96@gmail.com>
2024-06-14 15:24:55 +0200

    fix assistants component style

--- client/src/app/components/assistants-component/assistants-component.component.ts
+++ client/src/app/components/assistants-component/assistants-component.component.ts
@@ -1,135 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {Employee} from "../../employee";
-import {EmployeeService} from "../../services/employee.service";
+import { Component, OnInit } from '@angular/core';
+import { Employee } from "../../employee";
+import { EmployeeService } from "../../services/employee.service";
 import { FakerEmployeeDataService } from "../../faker-employee-data";
-import {EmployeeOperatingData} from "../../employee-operating-data";
+import { EmployeeOperatingData } from "../../employee-operating-data";
 import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-assistants-component',
   template: `
     <div class="widget-container" *ngFor="let employee of employees; let i = index">
-      <div class="widget green" [ngClass]="{'collapsed': isAccordionOpen[employee._id]}" *ngIf="employee._id && i < 2" >
+      <div class="widget green" *ngIf="employee._id && i < 2">
         <img class="icon" [src]="employee.img" matTooltip="{{ employee.position }}" [alt]="employee.name">
         <div class="value">
           <div class="top">{{ employee.name }}</div>
           <div class="bottom">{{ employee.level }}</div>
-          
-          
         </div>
-        <div class="data" *ngIf="employee._id && isAccordionOpen[employee._id]">
-          <div class="accordion-item">
-            <h2 class="accordion-header" id="accordion-header">
-              <button class="accordion-button less-padding" type="button" style="background-color:rgb(4,67,66);border:none;" (click)="closeAccordion(employee)">
-                <img src="../../assets/icons8-cloud-refresh.gif" alt="Icona"><p style="color:white;">Dati Realtime</p>
-              </button>
-            </h2>
-            <div class="accordion-collapse collapse show" id="accordion-collapse">
-              <div class="accordion-body less-padding">
-                <img src="../../assets/icons8-temperature-64.png" title="Temperatura" style="width: 20px; height: 20px;filter:invert(1);" alt="Temperature Icon" *ngIf="employee._id">
-                {{ accordionData[employee._id]?.temperature | number:'1.1-2' }}<br>
-                <img src="../../assets/icons8-saturation-96.png" title="Saturazione" style="width: 20px; height: 20px;filter:invert(1);" alt="Saturation Icon" *ngIf="employee._id">
-                {{ accordionData[employee._id]?.saturation | number:'1.1-2' }}<br>
-                <img src="../../assets/icons8-time-100.png" title="Data/Ora" style="width: 20px; height: 20px;filter:invert(1);" alt="Time Icon" *ngIf="employee._id">
-                {{ accordionData[employee._id]?.timeIn }}
-              </div>
+        <div class="data" *ngIf="employee._id">
+          <div class="accordion-body less-padding" style="gap: 0.2rem; background-color: transparent; color: white; display: flex; flex-direction: column;">
+            <div style="display: inline-flex; justify-content: space-between;">
+              <img src="../../assets/icons8-temperature-64.png" title="Temperatura" class="icon-small" alt="Icona Temperatura">
+              {{ accordionData[employee._id]?.temperature | number:'1.1-2' }}
             </div>
-          </div>
-        </div>
-        <ng-container *ngIf="employee._id">
-          <ng-container *ngIf="getOperatingData(employee._id) as operatingData">
-            <ng-container *ngFor="let data of operatingData">
-              <p id="dati-realtime" class="card-text custom-font" *ngIf="!employee._id || !isAccordionOpen[employee._id]">
-                <img src="../../assets/icons8-cloud-refresh.gif" style="width:30px;height:30px; margin-right:5px" alt="Icona" (click)="openAccordion(employee)">
-                <br>Dati Realtime
-              </p>
-            </ng-container>
-          </ng-container>
-        </ng-container>
-      </div>
-    </div>
-    <!--<div class="row">
-      <div class="col-md-6" *ngFor="let employee of employees; let i = index">
-        <div class="card bg-light" *ngIf="i < 2">
-          <div class="tooltip-container">
-            <img [src]="employee.img" style="width:80px; height:100px" class="card-img-top mx-auto d-block" matTooltip="{{ employee.position }}" [alt]="employee.name">
-          </div>
-          <div class="card-body less-padding">
-            <p class="card-text custom-font"> Nome: {{ employee.name }}</p>
-            <p class="card-text custom-font"> Livello: {{ employee.level }}</p>
-            <div class="accordion" *ngIf="employee._id && isAccordionOpen[employee._id]">
-              <div class="accordion-item">
-                <h2 class="accordion-header" id="accordion-header">
-                  <button class="accordion-button less-padding" type="button" (click)="closeAccordion(employee)">
-                    <img src="../../assets/icons8-cloud-refresh.gif" style="width:30px;height:30px; margin-right:5px" alt="Icona"> Dati Realtime
-                  </button>
-                </h2>
-                <div class="accordion-collapse collapse show" id="accordion-collapse">
-                  <div class="accordion-body less-padding">
-                  <img src="../../assets/icons8-temperature-64.png" title="Temperatura" style="width: 20px; height: 20px;" alt="Temperature Icon" *ngIf="employee._id">
-                  {{ accordionData[employee._id]?.temperature | number:'1.1-2' }}<br>
-                  <img src="../../assets/icons8-saturation-96.png" title="Saturazione" style="width: 20px; height: 20px;" alt="Saturation Icon" *ngIf="employee._id">
-                  {{ accordionData[employee._id]?.saturation | number:'1.1-2' }}<br>
-                  <img src="../../assets/icons8-time-100.png" title="Data/Ora" style="width: 20px; height: 20px;" alt="Time Icon" *ngIf="employee._id">
-                  {{ accordionData[employee._id]?.timeIn }}
-                </div>
-                </div>
-              </div>
+            <div style="display: inline-flex; justify-content: space-between;">
+              <img src="../../assets/icons8-saturation-96.png" title="Saturazione" class="icon-small" alt="Icona Saturazione">
+              {{ accordionData[employee._id]?.saturation | number:'1.1-2' }}
             </div>
-            <ng-container *ngIf="employee._id">
-              <ng-container *ngIf="getOperatingData(employee._id) as operatingData">
-                <ng-container *ngFor="let data of operatingData">
-                  <p id="dati-realtime" class="card-text custom-font" *ngIf="!employee._id || !isAccordionOpen[employee._id]">
-                    <img src="../../assets/icons8-cloud-refresh.gif" style="width:30px;height:30px; margin-right:5px" alt="Icona" (click)="openAccordion(employee)">
-                    Dati Realtime
-                  </p>
-                </ng-container>
-              </ng-container>
-            </ng-container>
+            <div style="display: inline-flex; justify-content: space-between;">
+              <img src="../../assets/icons8-time-100.png" title="Data/Ora" class="icon-small" alt="Icona Data/Ora">
+              {{ accordionData[employee._id]?.timeIn }}
+            </div>
           </div>
         </div>
       </div>
-    </div>-->
+    </div>
   `,
   styleUrls: ['../pro-version/top-widgets/top-widgets.component.scss']
 })
 export class AssistantsComponentComponent implements OnInit {
 
+  public employees: Employee[] = [];
+  public accordionData: { [id: string]: any } = {};
   private operatingdata: EmployeeOperatingData[] = [];
+
   constructor(private employeesService: EmployeeService,
               private fakerEmployeeOpData: FakerEmployeeDataService) { }
-
-  public employees: Employee[] = [];
-  public isAccordionOpen: { [id: string]: boolean } = {};
-  public accordionData: { [id: string]: any } = {};
-  private toastInfoSubject = new BehaviorSubject<{ show: boolean, message: string }>({ show: false, message: '' });
-  public toastInfo$ = this.toastInfoSubject.asObservable();
-  private alertTemperature = 37;
-  private alertSaturation = 92;
-
-  /*showToast(employeeId: string) {
-    const employeeOperatingData = this.operatingdata.find(item => item.id_employee === employeeId);
-
-    if (employeeOperatingData && employeeOperatingData.temperature !== null && employeeOperatingData.saturation !== null) {
-      if (employeeOperatingData.temperature > this.alertTemperature && employeeOperatingData.saturation < this.alertSaturation) {
-        this.toastInfoSubject.next({ show: true, message: 'This is a toast message!' });
-      } else {
-        this.toastInfoSubject.next({ show: false, message: '' });
-      }
-    } else {
-      // In caso di employeeOperatingData null o temperature/saturation null, nascondi il toast
-      this.toastInfoSubject.next({ show: false, message: '' });
-    }
-  }*/
-
-  /*Notifiche operatore
-      |> Cambia ricetta
-      |> Vai in pausa
-      |> Fine del turno
-      |> Vieni in cabina
-      |> Allarme antiincendio
-      |> Allarme generico
-   */
 
   ngOnInit(): void {
     this.employeesService.getEmployees().subscribe(
@@ -141,70 +55,22 @@ export class AssistantsComponentComponent implements OnInit {
         }
     );
 
-
-
-
-    this.employees = this.getEmployees();
-
     this.fakerEmployeeOpData.getDataObservable().subscribe((newData) => {
       this.operatingdata = newData;
-
-      for (let employeeId in this.isAccordionOpen) {
-
-        if (this.isAccordionOpen[employeeId]) {
-          this.updateEmployeeAccordionData(employeeId);
-        }
-      }
-
+      this.updateEmployeeData();
     });
-
   }
 
-  openAccordion(employee: any) {
-    this.isAccordionOpen[employee._id] = true;
-    this.updateEmployeeAccordionData(employee._id);
+  private updateEmployeeData() {
+    this.employees.forEach(employee => {
+      if (employee._id) {
+        this.updateEmployeeAccordionData(employee._id);
+      }
+    });
   }
 
-  updateEmployeeAccordionData(employeeId: string) {
+  private updateEmployeeAccordionData(employeeId: string) {
     const employeeOperatingData = this.operatingdata.filter(item => item.id_employee === employeeId);
     this.accordionData[employeeId] = employeeOperatingData.length > 0 ? employeeOperatingData[0] : null;
-  }
-
-  closeAccordion(employee: any): void {
-    this.isAccordionOpen[employee._id] = false;
-    this.accordionData[employee._id] = {};
-  }
-  getOperatingData(employeeId: string): EmployeeOperatingData[] {
-    return this.operatingdata.filter(item => item.id_employee === employeeId);
-  }
-
-  private getRandomOperators(): Employee[] {
-    if (this.employees.length < 2) {
-      throw new Error('Non ci sono abbastanza operatori nel database.');
-    }
-
-    const randomIndexes = this.getRandomIndexes(this.employees.length, 2); // Genera due indici casuali
-    const randomOperators = [this.employees[randomIndexes[0]], this.employees[randomIndexes[1]]];
-
-    return randomOperators;
-  }
-
-  public getEmployees() {
-    return this.employees;
-  }
-
-  toggleAccordion(index: number) {
-    this.isAccordionOpen[index] = !this.isAccordionOpen[index];
-  }
-
-  private getRandomIndexes(max: number, count: number): number[] {
-    const randomIndexes: number[] = [];
-    while (randomIndexes.length < count) {
-      const randomIndex = Math.floor(Math.random() * max);
-      if (!randomIndexes.includes(randomIndex)) {
-        randomIndexes.push(randomIndex);
-      }
-    }
-    return randomIndexes;
   }
 }
--- client/src/app/components/power-unit-charts/power-unit-charts.component.ts
+++ client/src/app/components/power-unit-charts/power-unit-charts.component.ts
@@ -1,18 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import {Chart} from "angular-highcharts";
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-power-unit-charts-component',
   templateUrl: './app-power-unit-charts.html',
-  styles: [
-  ]
+  styles: []
 })
 export class PowerUnitChartsComponent implements OnInit {
   labels = ['00:00 - 03:00', '03:00 - 06:00', '06:00 - 09:00', '09:00 - 12:00',
     '12:00 - 15:00', '15:00 - 18:00', '18:00 - 21:00', '21:00 - 24:00'];
 
-  hours = [90, 120, 212, 79, 92, 174, 75, 56]; // This corresponds to the "Hours" dataset
-  tons = [890, 789, 675, 666, 347, 0.00, 245, 645]; // This corresponds to the "Tons" dataset
+  hours = Array.from({ length: 8 }, () => Math.floor(Math.random() * 300)); // Random data for last 24 hours
+  tons = Array.from({ length: 8 }, () => Math.floor(Math.random() * 1000)); // Random data for last 24 hours
   chart: any;
 
   constructor() { }
@@ -20,7 +19,7 @@ export class PowerUnitChartsComponent implements OnInit {
   ngOnInit(): void {
     this.initializeChart(this.hours, this.tons);
     this.updateChartData();
-    setInterval(() => this.updateChartData(), 1800000); // Update every half hour
+    setInterval(() => this.updateChartData(), 20000); // Update every 20 seconds
   }
 
   initializeChart(hoursData: number[], tonsData: number[]): void {
@@ -33,20 +32,19 @@ export class PowerUnitChartsComponent implements OnInit {
         text: 'Tons produced over time'
       },
       xAxis: {
-        categories: ['00:00 - 03:00', '03:00 - 06:00', '06:00 - 09:00', '09:00 - 12:00',
-          '12:00 - 15:00', '15:00 - 18:00', '18:00 - 21:00', '21:00 - 24:00'],
+        categories: this.labels,
         title: {
-          text: 'hours'
+          text: 'Hours'
         }
       },
       yAxis: {
         title: {
-          text: 'tons'
+          text: 'Tons'
         }
       },
       series: [
         {
-          name: "Hours",
+          name: 'Hours',
           type: 'line',
           color: '#044342',
           data: hoursData
@@ -65,8 +63,51 @@ export class PowerUnitChartsComponent implements OnInit {
   }
 
   updateChartData(): void {
-    let hoursData = Array.from({ length: 8 }, () => Math.floor(Math.random() * 300));
-    let tonsData = Array.from({ length: 8 }, () => Math.floor(Math.random() * 1000));
-    this.initializeChart(hoursData, tonsData)
+    // Generate random data for the last 3 hours
+    let last3HoursHours = Array.from({ length: 1 }, () => Math.floor(Math.random() * 300));
+    let last3HoursTons = Array.from({ length: 1 }, () => Math.floor(Math.random() * 1000));
+
+    // Update the last 3 hours data in the existing arrays
+    this.hours[7] = last3HoursHours[0];
+
+    this.tons[7] = last3HoursTons[0];
+
+    this.chart = new Chart({
+      chart: {
+        type: 'line',
+        height: 450
+      },
+      title: {
+        text: 'Tons produced over time'
+      },
+      xAxis: {
+        categories: this.labels,
+        title: {
+          text: 'Hours'
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Tons'
+        }
+      },
+      series: [
+        {
+          name: 'Hours',
+          type: 'line',
+          color: '#044342',
+          data: this.hours
+        },
+        {
+          name: 'Tons',
+          type: 'line',
+          color: '#ed9e20',
+          data: this.tons
+        }
+      ],
+      credits: {
+        enabled: false
+      }
+    });
   }
 }
